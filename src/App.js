a058f07bb618ff096d01b72e037ee08212dd461a
Code Nation <codenation@Codes-MacBook-Pro.local>
2020-02-17 13:51:09 +0000

    added address book

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = {
+
+    text: "",
+    contacts: []
+  }
+
+  switchInput = (e) => {
+    this.setState({
+      text: e.target.value
+    })
+  }
+
+  addContact = () => {
+    console.log("clicked");
+    this.setState({
+      contacts: [...this.state.contacts, this.state.text]
+    })
+    
+  }
+
+  render() {
+
+    console.log(this.state.text);
+
+    const allContacts = this.state.contacts.map((contact) => {
+      return <h1>{contact}</h1>
+    })
+
+    const names = ["Simon", "John", "Peter"];
+
+    const allNames = names.map((name) => {
+      return <h1>{name}</h1>
+    });
+
+    // const allNames = ["<h1>Telmo</h1>", "<h1>John</h1>", "<h1>Peter</h1>"]
+
+    return (
+      <div>
+
+        <button onClick={this.addContact}>Add New Number</button>
+        <input type="text" onChange={this.switchInput} />
+
+        {allContacts}
+        {allNames}
+      </div>
+    )
+  }
 }
-
 export default App;
